48fe4ce66bfccd563094924eaa8f1bf4c7eb6ba8
sonka2598 <69012990+sonka2598@users.noreply.github.com>
2026-04-03 16:29:16 +0600

    Add files via upload

---
+++ Project8_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int count = 0;
+void counting_function()
+{
+	count += 1;
+	std::cout << "Количество вызовов функции counting_function(): " << count << "\n";
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	for (int i = 0; i < 15; i++)
+	{
+		counting_function();
+	}
+	return 0;
+}
---
+++ Project8_2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+namespace sum {
+    int calc(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+namespace diff {
+    int calc(int a, int b)
+    {
+        return a - b;
+    }
+}
+namespace multi {
+    int calc(int a, int b)
+    {
+        return a * b;
+    }
+}
+namespace divi {
+    double calc(int a, int b)
+    {
+        return (double)a / (double)b;
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int x = 0, y = 0;
+    std::cout << "x = ";
+    std::cin >> x;
+    std::cout << "y = ";
+    std::cin >> y;
+    std::cout << "Сложение: " << sum::calc(x, y);
+    std::cout << "\nВычитание: " << diff::calc(x, y);
+    std::cout << "\nУмножение: " << multi::calc(x, y);
+    std::cout << "\nДеление: " << divi::calc(x, y);
+    return 0;
+}
